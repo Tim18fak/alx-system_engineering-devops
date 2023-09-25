c152719f62394b9c01e6f95b5082e3b8190eade1
Tim18fak <Tim18fak@github.com>
2023-09-25 13:21:22 -0700

    web stack

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# fixes corrupted file with extension phpp
-exec { 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php':
-  path => '/usr/bin:/usr/sbin:/bin',
-}
--> exec {'restart service':
-  command => '/usr/sbin/service apache2 restart',
+# fixes corrupted file with extension php
+# # Substitutes `.phpp` with `.php` in the file `wp-settings.php`.
+
+exec { 'replace-phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
