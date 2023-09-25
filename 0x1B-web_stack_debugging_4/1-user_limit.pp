4fbc0fc53777f4a235721dbd23d1fefd1044ae2b
Tim18fak <Tim18fak@github.com>
2023-09-25 15:24:38 -0700

    web stack

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# Reconfigure the OS for 'holberton' to login and open a file without any error message
+
 exec { 'increase-hard-file-limit':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
